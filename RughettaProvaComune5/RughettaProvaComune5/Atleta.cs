87049771001a760769ad41011433b85e7d45b69b
MattiaRughetta <98026521+MattiaRughetta@users.noreply.github.com>
2022-02-17 13:25:09 +0100

    provacomune

---
+++ RughettaProvaComune5/RughettaProvaComune5/Atleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RughettaProvaComune5
+{
+    class Atleta
+    {
+        public string Nome { get; set; }
+        public string Società { get; set; }
+        public string Tempo { get; set; }
+        public string Città { get; set; }
+    }
+}
